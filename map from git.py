86c7852a59ca6de1c9ca55fdb3fcfba0d7c6d2e1
Malyshev Ilia <72250973+Mal-lab@users.noreply.github.com>
2020-10-30 14:03:15 +0300

    Add files via upload
    Добавлена карта для Северо-Западного федерального округа.
    Добавлен цвет маркера для 60 кВ

---
+++ map from git.py
@@ -0,0 +1,90 @@
+import folium
+from folium.plugins import MarkerCluster, Search, BeautifyIcon
+import pandas as pd
+from geojson import Feature, FeatureCollection, Point
+import json
+###Экспорт данных в GeoJson для использования в картах
+#Для подстанций
+df = pd.read_excel('data.xlsx')
+features = df.apply(
+    lambda row: Feature(properties={'name':row['Unnamed: 0'],
+                                    'ref':row['Ссылка'],
+                                    'Unom':row['Номинальное напряжение']},
+                        geometry=Point((float(row['Долгота']), float(row['Широта'])))), axis=1).tolist()
+feature_collection = FeatureCollection(features=features)
+
+with open('file.geojson', 'w', encoding='utf-8') as f:
+  json.dump(feature_collection, f, ensure_ascii = False)
+
+#Для объектов генерации
+df_gen = pd.read_excel('gen_data.xlsx')
+
+features_gen = df_gen.apply(
+    lambda row: Feature(properties={'name':row['Unnamed: 0'],
+                                    'ref':row['Ссылка']},
+                        geometry=Point((float(row['Долгота']), float(row['Широта'])))), axis=1).tolist()
+
+feature_collection_gen = FeatureCollection(features=features_gen)
+with open('file_gen.geojson', 'w', encoding='utf-8') as f:
+  json.dump(feature_collection_gen, f, ensure_ascii = False)
+#Построение карты
+#Выбор цвета взависимости от номинального напряжения
+def color_change(Unom):
+  U = [1150, 800, 750, 500, 400, 330, 220, 150, 110, 35, 20, 10, 6, 60]
+  colors = ['#CD8AFF', '#0000C8', '#0000C8', '#A50F0A', '#F0961E', '#008C00', '#C8C800', '#AA9600', '#00B4C8', '#826432', '#826432', '#640064', '#C89664', '#E533B5']
+  Unom_colors = dict(zip(U,colors))
+  if Unom not in U:
+      return 'black'
+  return Unom_colors[Unom]
+
+
+m = folium.Map(location=[63, 92], zoom_start=4)
+marker_cluster = MarkerCluster(name='Objects').add_to(m)
+#Создание слоев для ПС и генерации из geojson
+layer = folium.GeoJson(
+    feature_collection,
+    name='PS',
+    show=False
+).add_to(m)
+
+layer_gen = folium.GeoJson(
+    feature_collection_gen,
+    name='Generation',
+    show=False
+).add_to(layer)
+
+gj = folium.GeoJson('file.geojson')
+#Создание маркеров
+for feature in gj.data['features']:
+    folium.CircleMarker(
+        location=list(reversed(feature['geometry']['coordinates'])),
+        color='grey',
+        fill_color=color_change(feature['properties']['Unom']),
+        fill_opacity=1,
+        popup=folium.Popup(feature['properties']['name'] + '<p><a href=' + feature['properties'][
+            'ref'] + '>Ссылка на energybase.ru</a><p>', max_width=200),
+    ).add_to(marker_cluster)
+
+gj_gen = folium.GeoJson('file_gen.geojson')
+for feature in gj_gen.data['features']:
+    icon = BeautifyIcon(
+        icon='adjust',
+        inner_icon_style='color:black;font-size:30px;',
+        background_color='transparent',
+        border_color='transparent',
+    )
+    folium.Marker(
+        location=list(reversed(feature['geometry']['coordinates'])),
+        icon=icon,
+        popup=folium.Popup(feature['properties']['name'] + '<p><a href=' + feature['properties'][
+            'ref'] + '>Ссылка на energybase.ru</a><p>', max_width=200),
+    ).add_to(marker_cluster)
+#Добавление строки поиска
+substation_search = Search(layer=layer,
+                           geom_type='Point',
+                           placeholder='Search',
+                           search_label='name',
+                           search_zoom=11
+                           ).add_to(m)
+folium.LayerControl().add_to(m)
+m.save('SZFO.html')
---
+++ parsing from git.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+#Получение ссылок на каждый объект электроэнергетики
+#Выполняется отдельно для каждого фильтра
+def get_refs(number_of_pages,url_of_page): #41 для фильтра по Центральному федеральному округу (ПС)
+  list_of_refs = []
+  for page_number in range(1, number_of_pages + 1):
+    url  = url_of_page[:-1] + str(page_number)
+    page = requests.get(url)
+    new_soup = BeautifulSoup(page.content,'html.parser')
+    attempt = new_soup.find_all('div',{'class':'name'})
+    for stroka in attempt:
+      list_of_refs.append(str(stroka.find('a').get('href')))
+  return list_of_refs
+#Получение данных по заданному фильтру
+list_of_refs = get_refs(7, 'https://energybase.ru/power-plant/index?PowerPlantSearch%5Bcountry_id%5D=&PowerPlantSearch%5Bdistrict_id%5D=29&PowerPlantSearch%5Bname%5D=&PowerPlantSearch%5Btype_name%5D=&PowerPlantSearch%5Bcity_id%5D=&PowerPlantSearch%5Bstate%5D=&page=7')
+k = 0
+data = {}
+for ref in list_of_refs:
+  print('progress:',k+1,'/',len(list_of_refs))
+  url = 'https://energybase.ru' + ref
+  page = requests.get(url)
+  #Download page
+  soup = BeautifulSoup(page.content,'html.parser')
+  name_of_substation = soup.title.string
+  latitude = soup.find_all(string=re.compile('Широта',flags = re.I))
+  longitude = soup.find_all(string=re.compile('Долгота',flags = re.I))
+  if len(latitude) > 0 or len(longitude) > 0:
+    latitude = float(latitude[0].replace('Широта: ',''))
+    longitude = float(longitude[0].replace('Долгота: ',''))
+  data[name_of_substation] = {'Широта':latitude, 'Долгота':longitude, 'Ссылка':url}
+  name_of_substation
+  k += 1
+  exporting_data = pd.DataFrame(data).T
+  exporting_data.to_excel('gen_data.xlsx')
